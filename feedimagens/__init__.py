203fe7c4cc13d1e9d20d32b86fb83eae2f6a3fb7
will_iam <114028870+wdesouza95@users.noreply.github.com>
2023-08-25 10:44:15 -0300

    add db online

--- feedimagens/__init__.py
+++ feedimagens/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "90fd34e164badcf7bc75547b35f8451d"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts/"
 
